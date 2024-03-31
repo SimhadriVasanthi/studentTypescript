c54d73c6cf8cc83801854aca97994ad61fc0e47d
Simhadri Vasanthi <simhadrilakshmivasanthi@gmail.com>
2024-04-01 01:15:31 +0530

    feat:education slice

--- src/store/Slices/educationHistorySlice.ts
+++ src/store/Slices/educationHistorySlice.ts
@@ -1,5 +1,5 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
-import { EducationHistory,StoreItem } from "../../types/types";
+import { EducationHistory,StoreItem ,EducationHistory_Plus2, EducationHistory_School,EducationHistory_UnderGraduation,EducationHistory_PostGraduation } from "../../types/types"
 
 type EducationType="school" | "plus2" | "underGraduation" | "postGraduation"
 //type EducationData=EducationHistory_School | EducationHistory_Plus2 | EducationHistory_UnderGraduation | EducationHistory_PostGraduation
@@ -24,12 +24,12 @@ export const educationHistorySlice=createSlice({
         setSchool:(state,action:PayloadAction<EducationHistory_School>)=>{state.data.school=action.payload},
         setPlus2:(state,action:PayloadAction<EducationHistory_Plus2>)=>{state.data.plus2=action.payload},
         setUnderGraduation:(state,action:PayloadAction<EducationHistory_UnderGraduation>)=>{state.data.underGraduation=action.payload},
-        setPostGraduationl:(state,action:PayloadAction<EducationHistory_PostGraduation>)=>{state.data.postGraduation=action.payload},
+        setPostGraduation:(state,action:PayloadAction<EducationHistory_PostGraduation>)=>{state.data.postGraduation=action.payload},
         removeEducationHistory:(state,action:PayloadAction<EducationType>)=>{state.data[action.payload]=undefined},
         //setEducationHistory:(state,action:PayloadAction<{type:EducationType,data:any}>)=>{state.data[action.payload.type]=action.payload.data},
     }
 });
 
-export const {initEducationHistory,removeEducationHistory,setSchool,setPlus2,setPostGraduationl,setUnderGraduation}=educationHistorySlice.actions;
+export const {initEducationHistory,removeEducationHistory,setSchool,setPlus2,setPostGraduation,setUnderGraduation}=educationHistorySlice.actions;
 export default educationHistorySlice.reducer;
 
